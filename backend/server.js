b08f23b61f471919fc2c4ab76020d7345d87a79a
selaguardia <selaguardia91@gmail.com>
2023-01-26 08:59:19 -0800

    adds mongoose and successfully connects to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,34 @@
 const express = require("express");
-require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3000;
+require("dotenv").config();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const workoutRoutes = require("./routes/workouts");
+const port = process.env.PORT || 3000;
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use(morgan("dev"));
-
 
 // routes
 app.use("/api/workouts", workoutRoutes);
 
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(
+      "\x1b[36m%s\x1b[0m",
+      `🟢 🙌  [${new Date().toLocaleTimeString()}] - MongoDB connected ...  🙌 🟢`
+    );
+  })
+  .catch((err) => {
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      `❌😥❌ [${new Date().toLocaleTimeString()}] - MongoDB connection error ❌😥❌`
+    );
+    console.log(err.message);
+  });
 
 app.listen(port, () => {
   console.log(`✅ Listening for client requests on Port: ${port} ✅`);
