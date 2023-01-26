41b1ba1ed79b3bb95e79b4de9f66ba8cc47a641a
selaguardia <selaguardia91@gmail.com>
2023-01-26 09:15:44 -0800

    adds ability to create, read, update, and delete workouts from server to db

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,73 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
 
 // GET all workout
-router.get("/", (req,res)=> {
-    res.json({msg:'GET all workouts'})
-});
+router.get("/", async (req,res)=> {
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+  
+    res.status(200).json(workouts);
+  });
 
 // GET a single workout
-router.get("/:id", (req,res)=> {
-    res.json({msg:'GET single workout'})
-});
+router.get("/:id", async (req,res)=>  {
+    const { id } = req.params;
+  
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({error:"ID not valid"})
+    }
+    const workout = await Workout.findById(id);
+    if(!workout) {
+      return res.status(404).json({error:"That workout doesn't exist"})
+    }
+    res.status(200).json(workout);
+  });
 
 // POST a new workout
-router.post('/', (req,res)=> {
-    res.json({msg:'POST a  new workout'})
+router.post('/', async (req,res)=> {
+    const {title, weight, reps} = req.body;
+
+    try {
+      const workout = await Workout.create({title, weight, reps})
+      res.status(200).json(workout)
+    } catch (error) {
+      res.status(400).json({error: error.message})
+    }
 })
 
 // DELETE a workout
-router.delete('/:id', (req,res)=> {
-    res.json({msg:'Delete a workout'})
-})
+router.delete('/:id', async (req,res)=>  {
+    const { id } = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({error:"ID not valid"})
+    }
+    const workout = await Workout.findOneAndDelete({_id: id})
+  
+    if(!workout) {
+      return res.status(404).json({error:"That workout doesn't exist"})
+    }
+    res.status(200).json(workout)
+  
+  })
 
 // UPDATE a workout
-router.patch('/:id',(req,res)=> {
-    res.json({msg:'UPDATE a workout'})
-})
+router.patch('/:id',async (req,res)=> {
+    const { id } = req.params;
+  
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({error:"ID not valid"})
+    }
+  
+    const workout = await Workout.findOneAndUpdate({_id:id}, {
+      ...req.body
+    })
+    if(!workout) {
+      return res.status(404).json({error:"That workout doesn't exist"})
+    }
+  
+    res.status(200).json(workout)
+  })
 
 module.exports = router;
