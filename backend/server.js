50fd368fef484eaaaac69a3070fbfec559537e4b
selaguardia <selaguardia91@gmail.com>
2023-01-26 08:22:45 -0800

    adds morgan middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 3000;
+const morgan = require("morgan");
 
-app.get('/', (req, res) => {
-    res.json({message: 'Welcome to my app'})
-})
+// middleware
+app.use(morgan("dev"));
 
-app.listen(port,() => {
-    console.log(`✅ Listening for client requests on Port: ${port} ✅`)
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my app" });
+});
+
+app.listen(port, () => {
+  console.log(`✅ Listening for client requests on Port: ${port} ✅`);
+});
