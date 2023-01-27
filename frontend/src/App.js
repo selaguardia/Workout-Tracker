11e06d6d52b7800025b7df831210d4c80943f182
selaguardia <selaguardia91@gmail.com>
2023-01-27 12:12:29 -0800

    adds pages, components, and CSS style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// pages & components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>HELLO SERG</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
