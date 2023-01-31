a405084c1eb94f85bdb54a17e017a9637c262642
selaguardia <selaguardia91@gmail.com>
2023-01-31 09:06:51 -0800

    adds static method for signup and hashes password before creating user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,25 +1,44 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-},{timestamps: true});
+  { timestamps: true }
+);
+
+// static signup method
+userSchema.statics.signup = async (firstName, lastName, email, password) => {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Sorry, email is already in use.");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({firstName, lastName, email, password: hash});
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
