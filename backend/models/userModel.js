96db887405f667a8fce3b3c0c1dcd6567f5f4a79
selaguardia <selaguardia91@gmail.com>
2023-01-31 09:31:29 -0800

    adds validator to check for email and strong password, adds error messages

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -28,7 +29,17 @@ const userSchema = new Schema(
 
 // static signup method
 userSchema.statics.signup = async function(firstName, lastName, email, password) {
-    
+  // validation
+  if(!firstName || !lastName || !email || !password) {
+    throw Error("Sorry, all fields must be filled.")
+  }
+  if(!validator.isEmail(email)) {
+    throw Error("Sorry, please enter a valid email.")
+  }
+  if(!validator.isStrongPassword(password)){
+    throw Error("Sorry, your password is not strong enough.")
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
@@ -39,7 +50,9 @@ userSchema.statics.signup = async function(firstName, lastName, email, password)
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({firstName, lastName, email, password: hash});
+
   return user;
+
 };
 
 module.exports = mongoose.model("User", userSchema);
