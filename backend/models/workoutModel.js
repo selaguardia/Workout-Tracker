29e35f231ec72952aa73ab5ca7219ca350c31f76
selaguardia <selaguardia91@gmail.com>
2023-02-01 13:33:33 -0800

    adds user id assignment to each workout

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -14,6 +14,10 @@ const workoutSchema = new Schema({
     type: Number,
     required: true,
   },
+  user_id: {
+    type: String,
+    require: true
+  }
 }, {timestamps: true});
 
 module.exports = mongoose.model('Workout', workoutSchema);
